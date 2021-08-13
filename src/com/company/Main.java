04559ac81693e6c124fa00622aec38586ac37828
Simon Morris <simonjmorris213@gmail.com>
2021-08-12 18:55:46 -0700

    CLI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+
+        String userName = userIn.nextLine();
+        System.out.println("Hello " + userName);
     }
 }

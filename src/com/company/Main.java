4bc37028f62ac8de2565b0f0ac98f56a7cd55ca6
Simon Morris <simonjmorris213@gmail.com>
2021-08-12 18:56:28 -0700

    CLI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner userIn = new Scanner(System.in);
         System.out.println("Enter your name: ");
 

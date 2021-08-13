fa48ba1798c05f7c7f0ae7ff6852998f4ceb07ac
Simon Morris <simonjmorris213@gmail.com>
2021-08-12 20:24:32 -0700

    CLI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.print("Enter your name: ");
 
         String userName = userIn.nextLine();
-        System.out.println("Hello " + userName);
+        System.out.println("Hello " + userName + " Welcome to the CLI.\nPlease choose an option to continue:\n1. Say Hello\n2. Reverse a String\n3. Add two numbers\n4. Quit the program");
     }
 }

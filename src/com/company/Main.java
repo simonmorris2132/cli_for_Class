03c5866e85db6d370a4198abd0a0a470033d5e09
Simon Morris <simonjmorris213@gmail.com>
2021-08-12 19:14:16 -0700

    CLI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner userIn = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+        System.out.print("Enter your name: ");
 
         String userName = userIn.nextLine();
         System.out.println("Hello " + userName);

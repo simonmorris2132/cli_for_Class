f00c623ac4fafda99dfb87befb621bdb0df20e79
Simon Morris <simonjmorris213@gmail.com>
2021-08-16 19:07:36 -0700

    CLI

--- src/com/company/Option2.java
+++ src/com/company/Option2.java
@@ -9,14 +9,12 @@ public class Option2 {
         System.out.println("What string would you like to reverse?");
         String yourOption = Main.userIn.nextLine();
         String newOption = "";
-        System.out.println(yourOption);
 
 
         for (int i = yourOption.length() - 1; i >= 0; i--) {
-            System.out.println(yourOption.charAt(i));
-            newOption = String.valueOf(yourOption.charAt(i));
+            newOption += yourOption.charAt(i);
         }
-
+        System.out.println(newOption);
     }
 }
 
--- src/com/company/Option3.java
+++ src/com/company/Option3.java
@@ -3,11 +3,11 @@ package com.company;
 public class Option3 {
 
     public static void addTwoNum() {
-        System.out.println("What numbers would you like to add together: ");
-        int urOption = Main.userIn.nextInt();
+        System.out.println("Select number: ");
+        int x = Main.userIn.nextInt();
+        System.out.println("Select Second number: ");
+        int y = Main.userIn.nextInt();
 
-        int x = urOption;
-        int y = urOption;
         int sum = x + y;
 
         System.out.println("The output of " + x + " and " + y + " is: " + sum);
